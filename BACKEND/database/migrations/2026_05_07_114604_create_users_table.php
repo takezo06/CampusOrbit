f692cc5c512873396da65a849db92c3534af60fe
jepapica <yacobpapica@gmail.com>
2026-05-12 12:13:32 +0800

    successfully installed filament and created an initlal dashboard

--- BACKEND/database/migrations/2026_05_07_114604_create_users_table.php
+++ BACKEND/database/migrations/2026_05_07_114604_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
             $table->string('name'); 
-            $table->string('username', 50)->unique();
+            $table->string('username', 50)->unique()->nullable();
             $table->string('email', 100)->unique();
             $table->string('password');
             $table->enum('role', ['passenger', 'driver', 'admin'])->default('passenger');
